9a0869b9ed22e6e7c7bb429106ea76d2c6ed89a3
BlastaRasta <will.watson@ravenspec.com>
2017-03-12 08:45:33 +0000

    Feature: added a server logger

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import morgan from 'morgan';
 import utils from './utils';
 
 const port = process.env.PORT || 3000;
 
 let app = express()
+
+// Server Logger	
+app.use(morgan('dev'));
 
 app.listen(port, utils.startUpMessage(port))
